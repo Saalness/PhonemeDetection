aa6212c22dc07c2eec13c66b03599db6bcb1dcf2
Saalness <55344407+Saalness@users.noreply.github.com>
2024-05-11 22:41:19 +0400

    Say words
    -added an end session button (will be linked to return to menu in future)
    -changed the say words code to iterate through the word list one by one and give the user a chance to pronounce each one and check pronunciation

--- Assets/Scripts/SayWords.cs
+++ Assets/Scripts/SayWords.cs
@@ -3,76 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using System.IO;
 
 public class SayWords : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI text;
-    [SerializeField] private TextMeshProUGUI prompt;
-    [SerializeField] public Image result;
-    [SerializeField] public Image attempt;
-    [SerializeField] private Button checkbutton;
-    [SerializeField] private Button promptbutton;
-    static string[] words = { "at", "mad", "sad", "dad", "sat", "mat" };
-    public List<string> wordlist = new List<string>(words);
-    string word;
-    int i;
+    [SerializeField] private TextMeshProUGUI text; // User input field
+    [SerializeField] private TextMeshProUGUI prompt; // Display current word
+    [SerializeField] public Image result; // Success image
+    [SerializeField] public Image attempt; // Fail image
+    [SerializeField] private Button checkButton; // Check input button (I suggest that you rename it for clarity)
+    [SerializeField] private Button promptButton; // Next word button
+    [SerializeField] private Button stopButton; // Button to stop the exercise
 
-    // Start is called before the first frame update
+    static string[] words = { "at", "mad", "sad", "dad", "sat", "mat" };
+    private List<string> wordlist = new List<string>(words);
+    private int currentIndex = 0; // You need this to keep track of the current word index
+
     void Start()
     {
-        //result = GetComponent<UnityEngine.UI.Image>();
         result.enabled = false;
         attempt.enabled = false;
-        prompt.text = words[0];
-        //CheckList(0);
-        promptbutton.onClick.AddListener(NextPrompt);
-        checkbutton.onClick.AddListener(CheckList);
+        prompt.text = words[0]; // Start with the first word
+        promptButton.onClick.AddListener(NextPrompt);
+        checkButton.onClick.AddListener(CheckWord);
+        stopButton.onClick.AddListener(StopExercise);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
     void NextPrompt()
     {
-        result.enabled = false;
-        attempt.enabled = false;
-        word = wordlist[i];
-        prompt.text = word;
-    }
-    void CheckList()
-    {
-        if(text.text != "Sending..." || text.text != "Recording...") { CheckWord();
-        if (i < wordlist.Count) { CheckList(); }
-        
-        /**for (int i = 0; i < wordlist.Count; i++)
+        if(currentIndex < wordlist.Count - 1)
         {
-            word = wordlist[i];
-            prompt.text = word;
-            button.onClick.AddListener(CheckWord);
-
-            //CheckWord();
-        }**/
-        prompt.text = "well done";}
-        
+            currentIndex++;
+            prompt.text = words[currentIndex];
+            result.enabled = false;
+            attempt.enabled = false;
+        }
+        else
+        {
+            prompt.text = "well done";
+        }
     }
+
     void CheckWord()
     {
-        if(text.text.ToLower() == word)
+        if(text.text.ToLower() == wordlist[currentIndex].ToLower())
         {
             result.enabled = true;
+            attempt.enabled = false;
         }
-        else { attempt.enabled = true; }
-        i++;
+        else
+        {
+            result.enabled = false;
+            attempt.enabled = true;
+        }
     }
-    private void AddWord(string word)
+
+    void StopExercise()
     {
-        wordlist.Add(word);
-    }
-    private void DelWord(string delWord)
-    {
-        if (delWord != null)
-            wordlist.Remove(delWord);
+        // Here you can see the logic to execute when the stop button is pressed
+        prompt.text = "Session stopped";
+        result.enabled = false;
+        attempt.enabled = false;
+        // Hanin, this is for optionally reseting the currentIndex or disable buttons
     }
 }
